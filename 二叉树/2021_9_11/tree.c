b90ebde43f457fd7c7d8c43903285ba608da2df7
bruin-du <1769426648@qq.com>
2021-09-20 09:25:10 +0800

    前中后序遍历二叉树

---
+++ 二叉树/2021_9_11/tree.c
@@ -0,0 +1,67 @@
+#include"tree.h"
+
+BTNode* BTNodeInit(BTDataType x)
+{
+	BTNode* root = (BTNode*)malloc(sizeof(BTNode));
+	root->data = x;
+	root->left = root->right = NULL;
+	return root;
+}
+
+// 前序 中序 后序遍历
+void PrevOrder(BTNode* root)
+{
+	if (root == NULL)
+	{
+		return;
+	}
+	printf("%c ", root->data);
+	PrevOrder(root->left);
+	PrevOrder(root->right);
+}
+void InOrder(BTNode* root)
+{
+	if (root == NULL)
+	{
+		return;
+	}
+	PrevOrder(root->left);
+	printf("%c ", root->data);
+	PrevOrder(root->right);
+}
+void PostOrder(BTNode* root)
+{
+	if (root == NULL)
+	{
+		return;
+	}
+	PrevOrder(root->left);
+	PrevOrder(root->right);
+	printf("%c ", root->data);
+}
+// 节点个数
+int TreeSize(BTNode* root)
+{
+	if (root != NULL)
+	{
+		return TreeSize(root->left) + TreeSize(root->right) + 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+// 叶子节点的个数
+int TreeLeafSize(BTNode* root)
+{
+	if (root->left == NULL&&root->right == NULL)
+	{
+		return 1;
+	}
+	else
+	{
+		return TreeLeafSize(root->left) + TreeLeafSize(root->right);
+	}
+}
+// 层序遍历
+void LevelOrder(BTNode* root);
---
+++ 二叉树/2021_9_11/tree.h
@@ -0,0 +1,24 @@
+#pragma once
+#include<stdio.h>
+#include<windows.h>
+
+typedef char BTDataType;
+typedef struct BTNode
+{
+	struct BTNode* left;
+	struct BTNode* right;
+	BTDataType data;
+}BTNode;
+
+BTNode* BTNodeInit(BTDataType x);
+
+// 前序 中序 后序遍历
+void PrevOrder(BTNode* root);
+void InOrder(BTNode* root);
+void PostOrder(BTNode* root);
+// 节点个数
+int TreeSize(BTNode* root);
+// 叶子节点的个数
+int TreeLeafSize(BTNode* root);
+// 层序遍历
+void LevelOrder(BTNode* root);
